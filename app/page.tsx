03220d7f4913e98e54403e271a4194f2a938a689
bodyworkself <bodywork.self@gmail.com>
2025-10-14 21:51:02 +0900

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+// 既存の巨大な画面は app/prognosis-prediction/page.tsx にあります。
+// ここではそれをトップページから読み込んで出すだけ。
+import PrognosisPage from './prognosis-prediction/page'
+
+export default function Page() {
+  return <PrognosisPage />
+}
